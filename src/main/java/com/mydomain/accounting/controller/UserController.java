a283195c24b70dd83d535470eda9d17f6208a7d8
catmmao <catmmao@gmail.com>
2021-04-30 22:51:58 +0800

    feat(exception):参数错误的异常

--- src/main/java/com/mydomain/accounting/controller/UserController.java
+++ src/main/java/com/mydomain/accounting/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.mydomain.accounting.controller;
 
 import com.mydomain.accounting.converter.commonToService.UserInfoC2SConverter;
+import com.mydomain.accounting.exception.InvalidParameterException;
 import com.mydomain.accounting.model.common.UserInfoCommon;
 import com.mydomain.accounting.model.service.UserInfoService;
 import com.mydomain.accounting.service.User;
@@ -21,6 +22,10 @@ public class UserController {
 
     @GetMapping("/user/{id}")
     public ResponseEntity<UserInfoService> getUserInfo(@PathVariable int id) {
+        if (id <= 0) {
+            throw new InvalidParameterException("参数错误");
+        }
+
         UserInfoCommon userInfoCommon = user.getUserInfoById(id);
         return ResponseEntity.ok(userInfoC2SConverter.convert(userInfoCommon));
     }
---
+++ src/main/java/com/mydomain/accounting/exception/InvalidParameterException.java
@@ -0,0 +1,12 @@
+package com.mydomain.accounting.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class InvalidParameterException extends ServiceException {
+    public InvalidParameterException (String message) {
+        super(message);
+        this.setStatus(HttpStatus.BAD_REQUEST);
+        this.setErrorCode("INVALID_PARAMETER");
+        this.setErrorType(ErrorType.Client);
+    }
+}
