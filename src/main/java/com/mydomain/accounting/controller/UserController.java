2b7c80beed840188f27c0add03574f2894b7ffe9
catmmao <catmmao@gmail.com>
2021-04-30 22:17:10 +0800

    feat(controller):用户相关接口

---
+++ src/main/java/com/mydomain/accounting/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.mydomain.accounting.controller;
+
+import com.mydomain.accounting.converter.commonToService.UserInfoC2SConverter;
+import com.mydomain.accounting.model.common.UserInfoCommon;
+import com.mydomain.accounting.model.service.UserInfoService;
+import com.mydomain.accounting.service.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final User user;
+    private final UserInfoC2SConverter userInfoC2SConverter;
+
+    public UserController(User user, UserInfoC2SConverter userInfoC2SConverter) {
+        this.user = user;
+        this.userInfoC2SConverter = userInfoC2SConverter;
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<UserInfoService> getUserInfo(@PathVariable int id) {
+        UserInfoCommon userInfoCommon = user.getUserInfoById(id);
+        return ResponseEntity.ok(userInfoC2SConverter.convert(userInfoCommon));
+    }
+}
