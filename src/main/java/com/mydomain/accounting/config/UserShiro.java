5d15327bbffb9cd6a0d25db44eb39672b94a43f2
catmmao <catmmao@gmail.com>
2021-05-08 15:29:59 +0800

    chore(config):配置 shiro 的 realm

---
+++ src/main/java/com/mydomain/accounting/config/UserShiro.java
@@ -0,0 +1,36 @@
+package com.mydomain.accounting.config;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserShiro extends AuthorizingRealm {
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = (String) token.getPrincipal();
+        String password = new String((char[]) token.getCredentials());
+
+        if (username.isEmpty() || !username.equals("user")) {
+            throw new UnknownAccountException("找不到该用户");
+        }
+
+        if (password.isEmpty() || !password.equals("pwd")) {
+            throw new IncorrectCredentialsException("密码错误");
+        }
+
+        return new SimpleAuthenticationInfo(username, password, this.getName());
+    }
+}
