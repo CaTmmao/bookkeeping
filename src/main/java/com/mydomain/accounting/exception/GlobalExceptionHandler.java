b0af9f9a08849797bbe48c8b8829103a7c2b9379
catmmao <catmmao@gmail.com>
2021-05-10 15:16:39 +0800

    feat(exception): 处理密码错误 exception

--- src/main/java/com/mydomain/accounting/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mydomain/accounting/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.mydomain.accounting.exception;
 
+import static com.mydomain.accounting.exception.ServiceException.ErrorType.Client;
+
+import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,13 +13,29 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ServiceException.class)
     ResponseEntity<?> handleServiceException(ServiceException e) {
         ErrorResponse errorResponse =
-                new ErrorResponseBuilder()
-                        .statusCode(e.getStatus().value())
-                        .errorType(e.getErrorType())
-                        .errorCode(e.getErrorCode())
-                        .message(e.getMessage())
-                        .build();
+            new ErrorResponseBuilder()
+                .statusCode(e.getStatus().value())
+                .errorType(e.getErrorType())
+                .errorCode(e.getErrorCode())
+                .message(e.getMessage())
+                .build();
 
         return ResponseEntity.status(e.getStatus()).body(errorResponse);
+    }
+
+    // 密码错误
+    @ExceptionHandler(IncorrectCredentialsException.class)
+    ResponseEntity<?> handleInCorrectCredentialsException(IncorrectCredentialsException e) {
+        final int status = 400;
+
+        ErrorResponse errorResponse =
+            new ErrorResponseBuilder()
+                .statusCode(status)
+                .errorType(Client)
+                .errorCode("INCORRECT_CREDENTIALS")
+                .message(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(status).body(errorResponse);
     }
 }
